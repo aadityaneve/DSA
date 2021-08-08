54c7023177bd3e51d83eba7889a62e3e3a09bb8a
aadityaneve <aditya.neve@gmail.com>
2021-08-08 23:57:05 +0530

    added 'counts only as one try if they input the same number multiple times consecutively' added flag logic

--- Basic Problem Set/problem_8.js
+++ Basic Problem Set/problem_8.js
@@ -10,29 +10,46 @@ const prompt = ps({ sigint: true });
 
 var rand_num = Math.ceil(Math.random() * 9);
 var left_tries = 3;
+var old_num = -1;
+var flag = 0;
 
 console.clear();
-console.log("=====================");
+console.log("==========================================");
 console.log("--- Guessing Game ---");
-console.log("=====================");
+console.log("==========================================");
 do {
     var user_num = parseInt(prompt("Guess The Number : "));
-    left_tries--;
-    if (left_tries > 0) {
+    if (left_tries >= 1) {
+        left_tries--;
         if (user_num > rand_num) {
-            console.log("Too Large.");
-            console.log("Chances Left : "+left_tries);
+            console.log("\t\t\tToo Large.");
+            if (old_num == user_num) {
+                left_tries++;
+            }
+            console.log("Chances Left : " + left_tries);
         } else if (user_num < rand_num) {
-            console.log("Too Small");
-            console.log("Chances Left : "+left_tries);
+            console.log("\t\t\tToo Small");
+            if (old_num == user_num) {
+                left_tries++;
+            }
+            console.log("Chances Left : " + left_tries);
         } else if (user_num === rand_num) {
-            console.log(`You Guessed It Correctly!! The Number Is ${user_num}`);
+            flag = 1;
         }
     } else {
-        console.log("You Lost The Game!!");
-        console.log("The Number Was " + rand_num);
-        break;
-    }
 
-} while (user_num !== rand_num);
-console.log("=====================");
+        // break;
+    }
+    old_num = user_num;
+    // console.log(old_num);
+
+} while (user_num !== rand_num && left_tries != 0);
+
+if (flag == 0) {
+    console.log("==========================================");
+    console.log("You Lost The Game!!");
+    console.log("The Number Was " + rand_num);
+} else {
+    console.log(`You Guessed It Correctly!! The Number Is ${user_num}`);
+}
+console.log("==========================================");
