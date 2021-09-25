655b3888a684077024405d5e358cbb56da92af02
aadityaneve <aditya.neve@gmail.com>
2021-09-25 21:51:49 +0530

    Problem explanation added

--- OJ/king-and-collection-of-taxes.js
+++ OJ/king-and-collection-of-taxes.js
@@ -75,3 +75,64 @@ if (process.env.USERNAME === "aneve") {
         process.exit(0);
     });
 }
+
+
+
+/* 
+    King & Collection of Taxes - Editorial
+    Difficulty: Low
+    Prerequisite: Sorting
+    Problem Statement:
+    You are given an array of size2N, and you have to makeNpairs out of it, such that at each step, the smallest value not part of any other group, is paired with the largest value not part of any other group. The maximum value is taken from each group, and then the sum of all the values is to be printed
+
+    Hint:
+    How can you pair elements, according to the condition given in the question
+
+    Short Explaination
+    In this question, you are given an unsorted array, and you have to makeNpairs such that at each step, the smallest value not part of any other group, is paired with the largest value not part of any other group. The maximum value is taken from each group, and then the sum of all the values is to be printed. But, since we are only interested in the maximum values in each group, we can sort the array in descending order, and find the sum of values in the first half, which will be the required output
+
+    Detailed Explanation:
+    In this question, you are given an unsorted array, and you have to makeNpairs such that at each step, the smallest value not part of any other group, is paired with the largest value not part of any other group.
+
+    The maximum value is taken from each group, and then the sum of all the values is to be printed.
+
+    But, since we are only interested in the maximum values in each group, we can sort the array in descending order, and find the sum of values in the first half, which will be the required output
+
+    For example,`
+
+    [1 2 2 1 3 1]
+
+    After sorting this, it becomes
+
+    [1 1 1 2 2 3]Now, we can find the sum of the elements in the second half of the array which are 2 2 3 , because we are only interested in the maximum elements in each group. So, the elements will be 2 2 3 . So, their sum becomes 2 + 2 + 3 = 7 , which is the required answer
+
+    Another way to do this is, sort the array in descending order, so that the array becomes3 2 2 1 1 1
+
+    And now, we have to find the sum of elements in the first half of the array - so that the sum becomes 3 + 2 + 2 = 7 which is the required answer
+
+
+    - The constraints are such that the `Selection Sort` or `Bubble Sort` will result in a TLE. Therefore, in built sort must be used
+
+
+    ### **Pseudo Code**
+
+    ```java
+    function findTax(arr[], N){
+        //sort the array in descending order
+        sum = 0;
+        //Since array size is 2N, then half of it will be N
+        for (i = 0;i<N;i++){
+            sum += arr[i];
+        }
+        print (sum)
+    }
+
+    Time Complexity
+    The time complexity of the inbuilt sort isO(NlogN), which will be the required time complexity
+
+    Space Complexity
+    No extra space is required, therefore, the time complexity will be O(1).
+
+    Alternate Solution:
+    None.
+*/
