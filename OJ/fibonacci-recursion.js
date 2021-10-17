fa41805538d3f683938f8b40b854602c3eb183c3
Aditya Neve <aditya.neve@gmail.com>
2021-10-17 19:19:31 +0530

    editorial added

--- OJ/fibonacci-recursion.js
+++ OJ/fibonacci-recursion.js
@@ -81,3 +81,69 @@ if (process.env.USERNAME === "aneve") {
         process.exit(0);
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+/* 
+    Fibonacci - Recursive
+    Difficulty: Easy
+
+    Prerequisite: Recursion
+
+    Problem Statement:
+
+    Given an Integer n, you need to compute the nth fibonacci number using following reccurence relation
+
+    F(n) = F(n-1) + F(n-2) ; n>1
+
+    F(n) = 1 ; n==1
+
+    F(n) = 0 ; n==0
+
+    Hint:
+
+    None
+
+    Short Explanation:
+
+    None
+
+    Detailed Explanation:
+
+    just check for the base case, if n==0 then simply return 0 or if n==1 then return 1
+    other wise, take the sum of return values of functions which called with n-1 and n-2 recursively.
+    Pseudo code:
+
+    //n: integer
+    function nthFibonocci(n)
+        {
+            if (n==0)
+                return 0;
+            if (n==1)
+                            return 1;
+            return nthFibonocci(n-1) + nthFibonocci(n-2)
+        }
+    Time Complexity:
+
+    total 2^nfunction calls will be made, so at each and every call one addition is happening which is constant time. so the time complexity will beO(2^n)
+
+    Space Complexity:
+
+    Each and every function call one activation record is going to be created in stack, so here the space complexity willnot beO(2^n).
+
+    actual the space complexity will be O(2^n)
+
+    so while calculating space complexity of any recursive program, we should see at any point of time what will be the maximum size of stack, which is equal to the maximum depth of recursion tree. Hence space complexity isO(n)
+
+    Alternate Solution:
+
+    None
+*/
