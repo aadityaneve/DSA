d32d2aa3b125683fef9080726e17d5848b77cdd6
Aditya Neve <aditya.neve@gmail.com>
2021-11-07 10:57:28 +0530

    tailwind syntax

---
+++ Recursion/tailwind-syntax.js
@@ -0,0 +1,32 @@
+
+
+
+1 2 3 4 5
+
+arr.slice(1)
+
+
+
+<Person person={person} />,<Person person={person} />,<Person person={person} />
+
+
+
+#agjd {
+
+}
+
+
+<div id="cart" class="tailwind"></div>
+
+
+let cart = document.getElementById('cart');
+
+cart.setAttribute('class','bg-gray-500');
+cart.classList.add('bg-gray-600')
+
+
+
+const classArray = [
+    "cl1", "cl2", "cl3"
+    ];
+cart.classList.add(...classArray);
