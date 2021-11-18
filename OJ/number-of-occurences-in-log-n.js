66f21ed55e782516534267a7b85dde6fb7742506
Aditya Neve <aditya.neve@gmail.com>
2021-11-18 13:23:02 +0530

    used: recursion

--- OJ/number-of-occurences-in-log-n.js
+++ OJ/number-of-occurences-in-log-n.js
@@ -1,3 +1,41 @@
+/* 
+    Number of Occurences in logn Ended
+    Description
+
+    You are given n different numbers and an integer k. Write a program that finds number of times k is present in log(n) time complexity.
+
+    NOTE: YOU MUST NOT USE BRUTE FORCE SOLUTION
+
+    WE KNOW THAT YOU KNOW BRUTE FORCE SOLUTION AND WANT YOU TRY THE LOGN SOLUTION
+
+    USING BRUTE FORCE/LINEAR SEARCH IN THIS CASE WOULD LEAD TO DISQUALIFICATION
+
+
+    Input
+    Input Format :
+
+    First line contains N and K
+
+    Second line contains N space separated sorted integers
+
+    Constraints :
+
+    N<100
+
+
+    Output
+    Print number of times that number is present in the given list
+
+
+    Sample Input 1 
+
+    6 3
+    2 3 3 3 6 9
+    Sample Output 1
+
+    3
+*/
+
 function numberOfOccurencesInLogN(arr, K) {
     let count = 0;
     function search(K, low, high) {
