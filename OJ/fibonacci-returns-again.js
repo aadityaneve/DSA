004eb93ae051c8c4eadf9b4fd66e50958bf86e76
Aditya Neve <aditya.neve@gmail.com>
2021-12-07 00:04:51 +0530

    used: DP (cache)

---
+++ OJ/fibonacci-returns-again.js
@@ -0,0 +1,50 @@
+function fibonacciReturnsAgain(number) {
+    const cache = [0, 1];
+    for (let i = 1; i < number; i++) {
+        cache.push(-1);
+    }
+    // console.log(cache);
+    // cache[4] = 10;
+    // console.log(cache[4])
+    // console.log(typeof cache[4]);
+
+    // let cache = {}
+    let index = number;
+    function getFibonacci(number) {
+        // console.log(typeof number)
+        // console.log(cache)
+        if (cache[number - 1] != -1) {
+            return cache[number - 1];
+        } else {
+            return cache[number - 1] =
+                getFibonacci(number - 1) +
+                getFibonacci(number - 2);
+        }
+    }
+    getFibonacci(number+1);
+    console.log(cache[cache.length-1]);
+}
+
+function runProgram(input) {
+    fibonacciReturnsAgain(+input);
+}
+if (process.env.USERNAME === "aneve") {
+    runProgram(`50`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
