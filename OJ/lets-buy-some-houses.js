648523fe116b9db7e03f4a23fa202d71b5669a55
Aditya Neve <aditya.neve@gmail.com>
2022-01-09 12:13:47 +0530

    question added

--- OJ/lets-buy-some-houses.js
+++ OJ/lets-buy-some-houses.js
@@ -1,3 +1,46 @@
+/* 
+
+Let's buy some houses -515:46:50
+Description
+
+You just won a lottery and you want to spend some money. So, you want to buy some houses.
+
+There is just one condition:
+
+To buy two houses, you need to buy the road connecting those two houses. Now, you are given a list of roads, bought by you. You need to tell how many houses will you end up buying?
+
+
+Input
+Input Format
+
+First line contains an integer T, denoting the number of test cases. The first line of each test case contains an integer E, denoting the number of roads. The next E lines contain two space separated integers X and Y, denoting that there is a road between house X and house Y.
+
+Constraints
+
+1 <= T <= 100
+
+1 <= E <= 1000
+
+1 <= X, Y <= 10000
+
+
+Output
+For each test case, you need to print the number of houses you will end up buying.
+
+
+Sample Input 1 
+
+1
+3
+1 2
+2 3
+1 3
+Sample Output 1
+
+3
+
+*/
+
 class Graph {
     constructor() {
         this.element = new Map();
