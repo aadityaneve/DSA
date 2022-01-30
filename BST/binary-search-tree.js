6f98a5b2fb44c9ce3d6ce578dc0439eb391e5293
Aditya Neve <aditya.neve@gmail.com>
2022-01-30 23:00:05 +0530

    binary search tree: insert function added

---
+++ BST/binary-search-tree.js
@@ -0,0 +1,55 @@
+const util = require('util');
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        let newNode = new Node(value);
+
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+
+        let current = this.root;
+        while (true) {
+            if (value === current.value) return undefined;
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else if (value > current.value) {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(8);
+tree.insert(3);
+tree.insert(15);
+tree.insert(20);
+
+
+console.log(
+    util.inspect(tree, { showHidden: false, depth: null, colors: true })
+);
